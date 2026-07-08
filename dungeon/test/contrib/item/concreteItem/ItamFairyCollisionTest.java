3bc28c7c4b361548745325c0866e42a9f48d1cb2
ReviveHD <148337572+ReviveHD@users.noreply.github.com>
2026-07-08 17:47:55 +0000

    Apply Spotless formatting

--- dungeon/test/contrib/item/concreteItem/ItamFairyCollisionTest.java
+++ dungeon/test/contrib/item/concreteItem/ItamFairyCollisionTest.java
@@ -1,14 +1,14 @@
 package contrib.item.concreteItem;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.mockConstruction;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import contrib.components.CollideComponent;
--- dungeon/test/contrib/item/concreteItem/ItemFairyDropTest.java
+++ dungeon/test/contrib/item/concreteItem/ItemFairyDropTest.java
@@ -3,11 +3,11 @@ package contrib.item.concreteItem;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import contrib.components.CollideComponent;
 import contrib.entities.WorldItemBuilder;
