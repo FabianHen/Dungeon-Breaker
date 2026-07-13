1c5a1fb2507cbab046191199c950a824e63659cc
crossi262 <mariusroesner15@gmail.com>
2026-07-13 23:33:22 +0200

    Added keypad testing structure

---
+++ dungeon/test/contrib/modules/keypadFolder/AddDigitTest.java
@@ -0,0 +1,34 @@
+package contrib.modules.keypadFolder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import contrib.modules.keypad.KeypadComponent;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class AddDigitTest {
+
+    @Test
+    void addDigitAddsDigit() {
+
+        KeypadComponent keypad =
+                new KeypadComponent(List.of(1,2,3,4), () -> {}, false);
+
+        keypad.addDigit(5);
+
+        assertEquals(List.of(5), keypad.enteredDigits());
+    }
+
+    @Test
+    void addDigitDoesNothingWhenUnlocked() {
+
+        KeypadComponent keypad =
+                new KeypadComponent(List.of(1,2,3,4), () -> {}, false);
+
+        keypad.isUnlocked(true);
+
+        keypad.addDigit(5);
+
+        assertEquals(0, keypad.enteredDigits().size());
+    }
+}
---
+++ dungeon/test/contrib/modules/keypadFolder/EnteredStringTest.java
@@ -0,0 +1,33 @@
+package contrib.modules.keypadFolder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import contrib.modules.keypad.KeypadComponent;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class EnteredStringTest {
+
+    @Test
+    void enteredStringReturnsEnteredDigits() {
+        KeypadComponent keypad = new KeypadComponent(
+                List.of(1,2,3,4),
+                new ArrayList<>(List.of(1,2)),
+                false,
+                false);
+
+        assertEquals("12", keypad.enteredString());
+    }
+
+    @Test
+    void enteredStringFillsMissingDigitsWithStars() {
+        KeypadComponent keypad = new KeypadComponent(
+                List.of(1,2,3,4),
+                new ArrayList<>(List.of(1,2)),
+                false,
+                true);
+
+        assertEquals("12**", keypad.enteredString());
+    }
+}
