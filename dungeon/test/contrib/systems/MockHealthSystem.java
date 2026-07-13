86ce6c916ea9ef7aa64c5af194ad13396d812601
mbeu98 <mikabeu@yahoo.de>
2026-07-13 12:40:51 +0200

    Add tests for activateDeathAnimation

--- dungeon/test/contrib/systems/MockHealthSystem.java
+++ dungeon/test/contrib/systems/MockHealthSystem.java
@@ -79,4 +79,17 @@ public class MockHealthSystem extends HealthSystem {
         .filter(hsd -> !hsd.hc().alreadyDead())
         .forEach(this::triggerOnDeathPublic);
   }
+
+  /**
+   * Exposes the protected activateDeathAnimation() method for testing.
+   *
+   * @param data HSData to process.
+   * @return processed HSData
+   */
+  public HSData activateDeathAnimationPublic(HSData data) {
+    return super.activateDeathAnimation(data);
+  }
+
 }
+
+
